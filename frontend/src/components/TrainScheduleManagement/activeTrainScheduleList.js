9dd0a32bebe8672eca14455c5da2577e60e55545
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-10-12 03:06:03 +0530

    modify train schedule active table

--- frontend/src/components/TrainScheduleManagement/activeTrainScheduleList.js
+++ frontend/src/components/TrainScheduleManagement/activeTrainScheduleList.js
@@ -1,33 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { viewAllTrainSchedules } from "../../services/util/trainScheduleManagement";
 import Header from "../shared/Header";
+import { Modal } from "react-bootstrap";
+import UpdateTrainSchedule from "./updateTrainSchedule";
 
 const ActiveTrainScheduleList = () => {
 	// const [employees, setEmployees] = useState([]);
 	// const [showEmp, setShowEmp] = useState(false);
 	// const [modalEmp, setEmp] = useState([]);
 	const [trainSchedules, setTrainSchedules] = useState([]);
+	const [ModalEmpUpdate, setModalEmpUpdate] = useState([]);
+	const [ModalEmpUpdateConfirm, setModalEmpUpdateConfirm] =
+		useState(false);
+
+	const [ModalEmpDelete, setModalEmpDelete] = useState([]);
+	const [ModalEmpDeleteConfirm, setModalEmpDeleteConfirm] =
+		useState(false);
+
+	const [ModalEmpActive, setModalEmpActive] = useState([]);
+	const [ModalEmpActiveConfirm, setModalEmpActiveConfirm] =
+		useState(false);
+
+		const openModalEmpUpdate = (data) => {
+			setModalEmpUpdate(data);
+			setModalEmpUpdateConfirm(true);
+		};
+
+		const openModalEmpDelete = (data) => {
+			console.log("delEmp");
+			setModalEmpDelete(data);
+			setModalEmpDeleteConfirm(true);
+		};
 
 	useEffect(() => {
 		async function getAllTrainSchedules() {
-		  try {
-			let respond = await viewAllTrainSchedules();
-			if (respond.data) {
-			  // Filter the data to get only schedules with isActive = true
-			  const activeTrainSchedules = respond.data.filter(schedule => schedule.isActive === true);
-			  setTrainSchedules(activeTrainSchedules);
-			  console.log(activeTrainSchedules);
-			} else {
-			  console.log("error");
+			try {
+				let respond = await viewAllTrainSchedules();
+				if (respond.data) {
+					// Filter the data to get only schedules with isActive = true
+					const activeTrainSchedules = respond.data.filter(
+						(schedule) => schedule.isActive === true,
+					);
+					setTrainSchedules(activeTrainSchedules);
+					console.log(activeTrainSchedules);
+				} else {
+					console.log("error");
+				}
+			} catch (error) {
+				console.log(error);
 			}
-		  } catch (error) {
-			console.log(error);
-		  }
 		}
-	  
+
 		getAllTrainSchedules();
-	  }, []);
-	  
+	}, []);
+
+
 	return (
 		<div className="container pt-2">
 			<div
@@ -111,54 +138,113 @@ const ActiveTrainScheduleList = () => {
 						</thead>
 						<tbody>
 							{trainSchedules.map((trainSchedule) => {
-							return (
-								<tr>
-									<td class="text-center">
-										{trainSchedule.trainName}
-									</td>
-									<td class="text-center">
-										{trainSchedule.arrivalStation}
-									</td>
-									<td class="text-center">
-										{trainSchedule.departureStation}
-									</td>								
-									<td class="text-center">
-										{trainSchedule.arrivalTime}
-									</td>
-									<td class="text-center">
-										{trainSchedule.departureTime}
-									</td>
-									<td class="text-center">
+								return (
+									<tr>
+										<td class="text-center">
+											{trainSchedule.trainName}
+										</td>
+										<td class="text-center">
+											{trainSchedule.arrivalStation}
+										</td>
+										<td class="text-center">
+											{
+												trainSchedule.departureStation
+											}
+										</td>
+										<td class="text-center">
+											{/* {trainSchedule.arrivalTime} */}
+											{trainSchedule.arrivalTime.slice(11, 19)}
+										</td>
+										<td class="text-center">
+											{/* {trainSchedule.departureTime} */}
+											{trainSchedule.departureTime.slice(11, 19)}
+										</td>
+										<td class="text-center">
 											<button
 												class="btn btn-warning btn-sm"
 												style={{
 													marginRight: "4px",
 												}}
-												// onClick={() =>
-												// 	openModalEmpUpdate(
-												// 		trainSchedule,
-												// 	)
-												// }
-												>
+												onClick={() =>
+													openModalEmpUpdate(
+														trainSchedule,
+													)
+												}
+											>
 												Update
 											</button>
-												<button
-													// onClick={() =>
-													// 	openModalEmpDelete(
-													// 		trainSchedule,
-													// 	)
-													// }
-													class="btn btn-danger btn-sm">
-													Cancel
-												</button>
-										
+											<button
+												onClick={() =>
+													openModalEmpDelete(
+														trainSchedule,
+													)
+												}
+												class="btn btn-danger btn-sm">
+												Cancel
+											</button>
 										</td>
-								</tr>
-							);
-						})}
+									</tr>
+								);
+							})}
 						</tbody>
 					</table>
 				</div>
+
+								{/* modal for update employee details */}
+								<Modal
+					show={ModalEmpUpdateConfirm}
+					onHide={() => setModalEmpUpdateConfirm(false)}
+					size="lg"
+					aria-labelledby="contained-modal-title-vcenter"
+					centered>
+					<UpdateTrainSchedule
+						data={ModalEmpUpdate}
+						onHide={() => setModalEmpUpdate(false)}
+					/>
+				</Modal>
+				{/* modal for delete employee details */}
+				<Modal
+					show={ModalEmpDeleteConfirm}
+					onHide={() => setModalEmpDeleteConfirm(false)}
+					size="md"
+					aria-labelledby="contained-modal-title-vcenter"
+					centered>
+					<Modal.Header closeButton>
+						<Modal.Title>Confirm Cancellation</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<p>
+							Are you sure you want to cancel this train
+							schedule ?
+						</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<div className="row">
+							<div className="col -6">
+								<button
+									type="submit"
+									className="btn btn-delete"
+									// onClick={() => {
+									// 	deleteEmployee(ModalEmpDelete);
+									// }}
+								>
+									Yes
+								</button>
+							</div>
+							<div
+								className="col-6 text-right"
+								onClick={() =>
+									setModalEmpDeleteConfirm(false)
+								}>
+								<button
+									type="reset"
+									className="btn btn-reset">
+									No
+								</button>
+							</div>
+						</div>
+					</Modal.Footer>
+				</Modal>
 			</div>
 		</div>
 	);
